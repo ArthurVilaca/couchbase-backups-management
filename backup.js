e47d1c68dfad5edfadb8176f27eda2537b6f0df3
ArthurVilaca <vilaca.arthur.f@gmail.com>
2019-10-31 19:02:16 -0300

    restore directory

--- backup.js
+++ backup.js
@@ -1,33 +1,63 @@
 require('dotenv').config()
 const fs = require('fs');
 const AWS = require('aws-sdk');
+const path = require("path");
 const execSync = require('child_process').execSync;
 
 const full_backup = `sudo /opt/couchbase/bin/cbbackup couchbase://${process.env.DB_HOST}:8091 /backup-events -m full --single-node -u ${process.env.DB_USER} -p '${process.env.DB_PASSWORD}' -b ${process.env.DB_BUCKET}`
 const incremental_backup = `sudo /opt/couchbase/bin/cbbackup couchbase://${process.env.DB_HOST}:8091 /backup-events -m diff --single-node -u ${process.env.DB_USER} -p '${process.env.DB_PASSWORD}' -b ${process.env.DB_BUCKET}`
 
-async function execute(type = 'full') {
+AWS.config.update({
+  accessKeyId: process.env.AWS_ACCESS_KEY,
+  secretAccessKey: process.env.AWS_ACCESS_SECRET
+});
 
+const execute = function (type = 'incremental') {
   console.log(`${type}_backup`)
-  code = execSync(full_backup, { encoding: 'utf8', maxBuffer: 50 * 1024 * 1024 });
-  console.log(code.toString())
-
-  // const fileContent = fs.readFileSync(fileName);
-
-  // // Setting up S3 upload parameters
-  // const params = {
-  //   Bucket: process.env.S3_BUCKET_NAME,
-  //   Key: new Date().toString(),
-  //   Body: fileContent
-  // };
-
-  // // Uploading files to the bucket
-  // s3.upload(params, function (err, data) {
-  //   if (err) {
-  //     throw err;
-  //   }
-  //   console.log(`File uploaded successfully. ${data.Location}`);
-  // });
+  code = execSync(incremental_backup, { encoding: 'utf8', maxBuffer: 50 * 1024 * 1024 });
+  console.log('->>>>>>>>>>', code)
 }
 
+const uploadDir = function (s3Path, bucketName) {
+  let s3 = new AWS.S3();
+  function walkSync(currentDirPath, callback) {
+    fs.readdirSync(currentDirPath).forEach(function (name) {
+      var filePath = path.join(currentDirPath, name);
+      var stat = fs.statSync(filePath);
+      if (stat.isFile()) {
+        callback(filePath, stat);
+      } else if (stat.isDirectory()) {
+        walkSync(filePath, callback);
+      }
+    });
+  }
+
+  walkSync(s3Path, function (filePath, stat) {
+    let bucketPath = filePath.substring(s3Path.length + 1);
+    let params = { Bucket: bucketName, Key: bucketPath, Body: fs.readFileSync(filePath) };
+    s3.putObject(params, function (err, data) {
+      if (err) {
+        console.log(err)
+      } else {
+        console.log('Successfully uploaded ' + bucketPath + ' to ' + bucketName);
+      }
+    });
+
+  });
+};
+
+// const clearDir = function (dirPath) {
+//   fs.readdir(dirPath, (err, files) => {
+//     if (err) throw err;
+
+//     for (const file of files) {
+//       fs.unlink(path.join(dirPath, file), err => {
+//         if (err) throw err;
+//       });
+//     }
+//   });
+// }
+
 execute()
+uploadDir('/backup-events', 'backup-couchbase-tests');
+// clearDir('/backup-events')
--- restore.js
+++ restore.js
@@ -1,3 +1,68 @@
-export async function execute() {
+require('dotenv').config()
+const AWS = require('aws-sdk');
+var async = require('async');
+var fs = require('fs');
+
+AWS.config.update({
+  accessKeyId: process.env.AWS_ACCESS_KEY,
+  secretAccessKey: process.env.AWS_ACCESS_SECRET
+});
+
+const execute = function () {
   `sudo /opt/couchbase/bin/cbrestore couchbase://${process.env.DB_HOST}:8091 /backup-events -u ${process.env.DB_USER} -p '${process.env.DB_PASSWORD}' -b ${process.env.DB_BUCKET}`
 }
+
+const listBackups = function (bucket_name) {
+  let s3 = new AWS.S3();
+  let params = { Bucket: bucket_name, Delimiter: '/', };
+
+  s3.listObjects(params, function (err, data) {
+    console.log(data)
+  })
+}
+
+const syncBackup = function (PREFIX, BUCKET) {
+  var params = {
+    Bucket: BUCKET,
+    Prefix: PREFIX
+  }
+
+  var s3 = new AWS.S3();
+  s3.listObjects(params, function (err, data) {
+    if (err) return console.log(err);
+    console.log(data)
+
+    async.eachSeries(data.Contents, function (fileObj, callback) {
+      var key = fileObj.Key;
+      console.log('Downloading: ' + key);
+
+      var fileParams = {
+        Bucket: BUCKET,
+        Key: key
+      }
+
+      s3.getObject(fileParams, function (err, fileContents) {
+        if (err) {
+          callback(err);
+        } else {
+          // Read the file
+          var contents = fileContents.Body.toString();
+          console.log(contents)
+
+          // Do something with file
+
+          callback();
+        }
+      });
+    }, function (err) {
+      if (err) {
+        console.log('Failed: ' + err);
+      } else {
+        console.log('Finished');
+      }
+    });
+  });
+}
+
+// listBackups('backup-couchbase-tests')
+syncBackup('2019-10-31T174050Z', 'backup-couchbase-tests')
